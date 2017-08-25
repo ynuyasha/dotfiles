b5033498cb5996e9b86384465028e4e8fc793b43
Jozef Reisinger <jozef.reisinger@gmail.com>
2017-08-25 16:02:50 +0200

    add support for -r regex

--- bin/myquote.py
+++ bin/myquote.py
@@ -4,23 +4,33 @@ import argparse
 import requests
 import random
 import time
+import re
 
 class MyQuote:
 
     def __init__(self):
         self.quotes = []
+        self.quote = ''
+        self.url = \
+         'https://raw.githubusercontent.com/jreisinger/blog/master/posts/quotes.txt'
 
     def download(self):
-        url = 'https://raw.githubusercontent.com/jreisinger/blog/master/posts/quotes.txt'
-        r = requests.get(url)
-        self.quotes = (r.text)
+        r = requests.get(self.url)
+        self.quotes = ( r.text.split('\n\n') )
 
-    def print_rand(self, slow):
-        quote = random.choice( self.quotes.split('\n\n') )
-        if slow:
-            self._slow_print(quote)
+    def pick(self, regex):
+        if regex:
+            regex_i = re.compile(regex, re.IGNORECASE)
+            quotes = filter( lambda q: re.search(regex_i, q), self.quotes )
+            self.quote = '\n\n'.join(quotes)
         else:
-            print(quote)
+            self.quote = random.choice( self.quotes )
+
+    def print_out(self, slow):
+        if slow:
+            self._slow_print(self.quote)
+        else:
+            print(self.quote)
 
     def _slow_print(self, quote):
         for letter in quote:
@@ -29,12 +39,15 @@ class MyQuote:
         print()
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Print a random quote from \
-                                        my collection of quotes.')
+    parser = argparse.ArgumentParser(description='Print a quote from my \
+                                     collection of quotes.')
     parser.add_argument('-s', action='store_true', help='print quote slowly')
+    parser.add_argument('-r', metavar='regex', help='print quotes matching \
+                        regex (case insensitive)')
     args = parser.parse_args()
 
     quotes = MyQuote()
     quotes.download()
-    quotes.print_rand(slow=args.s)
+    quotes.pick(args.r)
+    quotes.print_out(args.s)
 
