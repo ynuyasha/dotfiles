3c3a4a79e884df2cee4b5c347cc41abaf05ad386
Jozef Reisinger <jozef.reisinger@gmail.com>
2017-08-28 15:24:25 +0200

    implement caching to a local file

--- bin/myquote.py
+++ bin/myquote.py
@@ -5,20 +5,40 @@ import requests
 import random
 import time
 import re
+import pickle
+import os
 
 class MyQuote:
 
     def __init__(self):
-        self.quotes = []
-        self.quote = ''
+        self.quotes = [] # all quotes
+        self.quote = ''  # picked quotes
         self.url = \
          'https://raw.githubusercontent.com/jreisinger/blog/master/posts/quotes.txt'
+        self.cache = '/tmp/myquotes.data'
 
+    def get(self):
+        """ Read quotes from a local file. If that is too old download them.
+        """
+        cache_age = os.path.getmtime(self.cache)
+        now = time.time()
+        day_ago = now - 60*68*24*1
+        if cache_age < day_ago: # cache older than a day
+            self.download()
+        f = open(self.cache, 'rb')
+        self.quotes = pickle.load(f)
+            
     def download(self):
+        """ Download quotes from the Internet.
+        """
         r = requests.get(self.url)
-        self.quotes = ( r.text.split('\n\n') )
+        quotes = ( r.text.split('\n\n') )
+        f = open(self.cache, 'wb')
+        pickle.dump(quotes, f)
 
     def pick(self, regex):
+        """ Pick quotes matching a regex. Or a random quote.
+        """
         if regex:
             regex_i = re.compile(regex, re.IGNORECASE)
             quotes = filter( lambda q: re.search(regex_i, q), self.quotes )
@@ -28,11 +48,11 @@ class MyQuote:
 
     def print_out(self, slow):
         if slow:
-            self._slow_print(self.quote)
+            self.slow_print(self.quote)
         else:
             print(self.quote)
 
-    def _slow_print(self, quote):
+    def slow_print(self, quote):
         for letter in quote:
             print(letter, end='', flush=True)
             time.sleep(.05)
@@ -46,8 +66,8 @@ if __name__ == '__main__':
                         regex (case insensitive)')
     args = parser.parse_args()
 
-    quotes = MyQuote()
-    quotes.download()
-    quotes.pick(args.r)
-    quotes.print_out(args.s)
+    q = MyQuote()
+    q.get()
+    q.pick(args.r)
+    q.print_out(args.s)
 
